c87f069ce9df688be29bd9ac6f3a3ccb65a7af0f
Crolow <llequenne@hotmail.com>
2025-03-27 15:12:45 +0100

    Added Crolow.TopMachine a blazor scrabble app.

---
+++ Crolow.TopMachine/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Crolow.TopMachine
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
